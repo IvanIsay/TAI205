3c30a09399b970bd8d6448f3e8c210b5e426cdb5
Ivan Guerra <102552151+IvanIsay@users.noreply.github.com>
2026-01-29 16:10:17 -0600

    Se completo la Practica No1

---
+++ miAPI/main.py
@@ -0,0 +1,16 @@
+#pendiente EXPLICAR COMMMITS SIG clase
+
+#1. importaciones
+from fastapi import FastAPI
+
+#2.Inicializacion APP
+app= FastAPI()
+
+#3.Endpoints
+@app.get("/")
+async def holaMundo():
+    return {"mensaje":"Hola mundo FASTAPI"}
+
+@app.get("/bienvenidos")
+async def bien():
+    return {"mensaje":"Bienvenidos"}
