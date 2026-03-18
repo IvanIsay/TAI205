dad1e212921e7765645ffa48b272be6d7c648b6f
Ivan Guerra <102552151+IvanIsay@users.noreply.github.com>
2026-03-17 20:18:50 -0600

    auth

--- miAPI/app/main.py
+++ miAPI/app/main.py
@@ -1,33 +1,61 @@
-
-#1. importaciones
-from fastapi import FastAPI,status, HTTPException
-from typing import Optional
+#importaciones
+from fastapi import FastAPI, status, HTTPException,Depends
 import asyncio
-
+from typing import Optional
+from pydantic import BaseModel,Field
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import secrets
 
 #******************
-#2.Inicializacion APP
+#Instancia del servidor
 #******************
 
 app= FastAPI(
-    title=' MI Primer API',
+    title="Mi Primer API",
     description="Ivan Isay Guerra L",
-    version='1.0.0'
-    )
+    version="1.0"
+)
+
 
 #******************
-# BD ficticia
+#TB ficticia
 #******************
 
 usuarios=[
-    {"id":"1","nombre":"Ivan","edad":38},
-    {"id":"2","nombre":"Diana","edad":20},
-    {"id":"3","nombre":"Julian","edad":20},
+    {"id":1,"nombre":"Diego","edad":21},
+    {"id":2,"nombre":"Coral","edad":21},
+    {"id":3,"nombre":"Saul","edad":21},
 ]
 
+#******************
+#Modelo Pydantic de validacion
+#******************
+
+class crear_usuario(BaseModel):
+    id: int= Field(...,gt=0,description= "identificador de usuario")
+    nombre:str= Field(...,min_length= 3, max_length= 50, example="Juanito Doe")
+    edad:int = Field(..., ge=1, le=125,description="Edad validad entre 1 y 125" )   
 
 
-     
+#******************
+#Seguridad con HTTP BASIC
+#******************
+
+security= HTTPBasic()
+
+def verificar_peticion(credenciales:HTTPBasicCredentials=Depends(security)):
+    usuarioAut= secrets.compare_digest(credenciales.username,"ivanisay")
+    contraAuth= secrets.compare_digest(credenciales.password,"123456")
+    
+    if not(usuarioAut and contraAuth):
+        raise HTTPException(
+            status_code= status.HTTP_401_UNAUTHORIZED,
+            detail=" Credenciales no autorizadas"
+        )
+        
+    return credenciales.username  
+
+
 #******************
 #  Usuario CRUD
 #******************
@@ -41,9 +69,9 @@ async def leer_usuarios( ):
     }
     
 @app.post("/v1/usuarios/",tags=['CRUD HTTP'] ,status_code=status.HTTP_201_CREATED)
-async def crear_usuario(usuario:dict):
+async def crear_usuario(usuario:crear_usuario):
     for usr in usuarios:
-        if usr["id"] == usuario.get("id"):
+        if usr["id"] == usuario.id:
             raise HTTPException(
                 status_code=400,
                 detail="El id ya existe"
@@ -94,7 +122,7 @@ async def actualizar_parcial(id: int, datos: dict):
     )
     
 @app.delete("/v1/usuarios/{id}", tags=["CRUD HTTP"],status_code=status.HTTP_200_OK)
-async def eliminar_usuario(id: int):
+async def eliminar_usuario(id: int,usuarioAuth:str=Depends(verificar_peticion)):
 
     for index, usr in enumerate(usuarios):
         if usr["id"] == id:
@@ -102,7 +130,7 @@ async def eliminar_usuario(id: int):
             usuarios.pop(index)
 
             return {
-                "message": "Usuario eliminado correctamente"
+                "message": f"Usuario eliminado por {usuarioAuth}"
             }
 
     raise HTTPException(
@@ -111,54 +139,34 @@ async def eliminar_usuario(id: int):
     )
     
     
+#******************    
+# otros Endpoints
 #******************
-# Otros Endpoints
-#******************
-@app.get("/", tags=['Inicio'])
-async def holaMundo():
-    return {"mensaje":"Hola mundo FASTAPI"}
+@app.get("/",tags=['Inicio'])
+async def bienvenido():
+    return {"mensaje":"Bienvenido a FastAPI"}
 
-@app.get("/v1/bienvenidos", tags=['Inicio'])
-async def bien():
-    return {"mensaje":"Bienvenidos"}
-
-@app.get("/v1/promedio", tags=['Calificaciones'])
-async def promedio():
-    await asyncio.sleep(3) #simulacion peticion,consultaBD..
+@app.get("/holaMundo",tags=['Asincronia'])
+async def Hola():
+    await asyncio.sleep(5) #peticion,consultaBD,Archivo
     return {
-            "Calificacion":"7.5",
-            "estatus":"200"
-            }  
-    
-@app.get("/v1/parametroO/{id}", tags=['Parametro Obligatorio'])
-async def consultaUno(id:int):
-    await asyncio.sleep(3)
-    return {
-        "Resultado":"usuario encontrado",
-        "Estatus":"200",
+        "mensaje":"Hola Mundo FastAPI",
+        "status":"200"
         }
+            
+@app.get("/v1/ParametroOb/{id}",tags=['Parametro Obligatorio'])
+async def consultauno(id:int):
+    return {"mensaje":"usuario encontrado",
+            "usuario":id,
+            "status":"200" }
     
+@app.get("/v1/ParametroOp/",tags=['Parametro Opcional'])
+async def consultatodos(id:Optional[int]=None):
+    if id is not None:
+        for usuarioK in usuarios:
+            if usuarioK["id"] == id:
+                return{"mensaje":"usuario encontrado","usuario":usuarioK}
+        return{"mensaje":"usuario no encontrado","status":"200"}   
+    else:
+        return {"mensaje":"No se proporciono id","status":"200"} 
     
-    
-@app.get("/v1/usuarios/", tags=['CRUD HTTP'])
-async def consultaT():
-    return{
-        "status":"200",
-        "total": len(usuarios),
-        "data": usuarios
-    }
-    
-@app.post("/v1/usuarios/", tags=['CRUD HTTP'])
-async def crea_usuario(usuario:dict):
-    for usr in usuarios:
-        if usr["id"] == usuario.get("id"):
-            raise HTTPException(
-                status_code=400,
-                detail=" El id ya existe"
-            ) 
-    usuarios.append(usuario)
-    return{
-        "mensaje": "usuario agregado correctamente",
-        "status":"200",
-        "usuario":usuario
-    }
